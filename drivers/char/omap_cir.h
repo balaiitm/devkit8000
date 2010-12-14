7263831730bfb787eb5c02ffb74f1dede85904ee
Balamurugan <balaiitm@gmail.com>
2010-12-14 15:50:40 +0530

    base 2.6.37rc2 for devkit8000

---
+++ drivers/char/omap_cir.h
@@ -0,0 +1,75 @@
+
+#define UINT32   unsigned int
+#define UINT16   unsigned short
+#define UINT8     unsigned char
+#define INT32	    signed int
+#define INT16     signed short
+
+#define CIR_DEVICE "cir_dev"	
+#define CIR_MAJOR 250	
+#define TIMER_32K_SYNCHRONIZED         (OMAP3430_32KSYNCT_BASE + 0x10)       //Free running timer in omap
+
+#define ONE 	             60             //   60/TIMER_FREQUENCY  = 1.8   |```|_|
+#define ZERO	             40             //   40/TIMER_FREQUENCY  = 1.2   |`|_|
+#define BITS_COUNT   20
+#define TOLERANCE    10 //5
+
+
+/****************************/
+///****Keyboard Key map****///
+
+#define home	102
+#define up            103
+#define down        108
+#define left           105
+#define right         106
+#define enter        28
+#define esc           1
+#define window     126
+#define b              48
+#define vol_up      104
+#define vol_down  109
+
+/****************************/
+#define key1    2
+#define key2    3
+#define key3    4
+#define key4    5
+#define key5    6
+#define key6    7
+#define key7    8
+#define key8    9
+#define key9    10
+#define key0    11
+#define keyA    30
+#define keyB    48
+#define keyC    46
+#define keyD    32
+#define keyE    18
+#define keyF     33
+#define keyG    34
+#define keyH    35
+#define keyI      23
+#define keyJ      36
+#define keyK     37
+#define keyL     38
+#define keyM    50
+#define keyN    49
+#define keyO    24
+#define keyP    25
+#define keyQ    16
+#define keyR    19
+#define keyS    31
+#define keyT    20
+#define keyU    22
+#define keyV    47
+#define keyW   17
+#define keyX    45
+#define keyY    21
+#define keyZ   44
+#define keydot  52
+#define keyback  14
+#define keyspace  57
+#define keyshift 42
+
+
---
+++ include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 44 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#44 Sat Dec 4 14:22:42 IST 2010"
+#define LINUX_COMPILE_TIME "14:22:42"
+#define LINUX_COMPILE_BY "nikhil"
+#define LINUX_COMPILE_HOST "nikhil-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.3.3 (GCC) "
